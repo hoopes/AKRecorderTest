30bf839af1dd7614001dfc64bb2ef83408fe5b73
Hoopes <matthew.hoopes@gmail.com>
2021-12-08 20:17:33 -0500

    Initial Commit

---
+++ AKRecorderTest/AKRecorderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AKRecorderTestApp.swift
+//  AKRecorderTest
+//
+//  Created by Matthew Hoopes on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AKRecorderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AKRecorderTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AKRecorderTest
+//
+//  Created by Matthew Hoopes on 12/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
