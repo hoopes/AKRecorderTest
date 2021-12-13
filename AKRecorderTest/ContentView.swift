eab08576b192eae636030cd8900a1515c40df840
Hoopes <matthew.hoopes@gmail.com>
2021-12-13 15:31:54 -0500

    Updated to mimic the test code exactly

--- AKRecorderTest/ContentView.swift
+++ AKRecorderTest/ContentView.swift
@@ -10,74 +10,19 @@ import AudioKitEX
 import AVFoundation
 import SwiftUI
 
-struct RecorderData {
-  var isRecording = false
-  var isPlayingInput = false
-  var isPlayingRecording = false
-}
-
 class RecorderConductor: ObservableObject {
-  let engine = AudioEngine()
-  var inputPlayer = AudioPlayer()
-  var recorder: NodeRecorder?
-  let recordingPlayer = AudioPlayer()
-  let mixer = Mixer()
-
-  // Two example input audio files, different formats
-  let mp3Url = Bundle.main.url(forResource: "alphabet", withExtension: "mp3")
-  let wavUrl = Bundle.main.url(forResource: "alphabet", withExtension: "wav")
-
-  @Published var data = RecorderData() {
-    didSet {
-      if data.isRecording {
-        NodeRecorder.removeTempFiles()
-        do {
-          try recorder?.record()
-        } catch let err {
-          print(err)
-        }
-      } else {
-        if let rec = recorder {
-          if rec.isRecording {
-            recorder?.stop()
-
-            print ("*** Created file: \(recorder?.audioFile?.url)")
-          }
-        }
-      }
-
-      if data.isPlayingInput {
-        if !inputPlayer.isPlaying {
-          inputPlayer.play()
-        }
-      }
-      else {
-        if inputPlayer.isPlaying {
-          inputPlayer.stop()
-        }
-      }
-
-      if data.isPlayingRecording {
-        if let file = recorder?.audioFile {
-          recordingPlayer.file = file
-          recordingPlayer.play()
-        }
-      } else {
-        if recordingPlayer.isPlaying {
-          recordingPlayer.stop()
-        }
-      }
-    }
-  }
 
   init() {
+    NodeRecorder.removeTempFiles() // just make sure the temp dir exists
 
-    // Set up an AudioPlayer with a file from disk
-    try! inputPlayer.load(url: mp3Url!)
-//    try! inputPlayer.load(url: wavUrl!)
-    inputPlayer.isLooping = true
+    let mp3Url = Bundle.main.url(forResource: "alphabet", withExtension: "mp3")!
+    let player = AudioPlayer(url: mp3Url)!
 
-    /// ------ Snip from test (https://github.com/AudioKit/AudioKit/blob/main/Tests/AudioKitTests/Extension%20Tests/AVAudioPCMBufferTests.swift#L20)
+    let engine = AudioEngine()
+    engine.output = player
+    try! engine.start()
+    player.play()
+
     let fm = FileManager.default
     let filename = UUID().uuidString + ".m4a"
     let outfileUrl = fm.temporaryDirectory.appendingPathComponent(filename)
@@ -90,54 +35,28 @@ class RecorderConductor: ObservableObject {
       forWriting: outfileUrl,
       settings: settings)
 
-    recorder = try! NodeRecorder(node: inputPlayer, file: outFile)
-    /// ------ End snip from test
+    let recorder = try! NodeRecorder(node: player, file: outFile)
+    try! recorder.record()
 
-    mixer.addInput(inputPlayer)
-    mixer.addInput(recordingPlayer)
-    engine.output = mixer
-  }
+    sleep(2)
 
-  func start() {
-    do {
-      try engine.start()
-    } catch let err {
-      print(err)
-    }
-  }
-
-  func stop() {
+    recorder.stop()
     engine.stop()
+
+    print ("*** Created m4a file: \(recorder.audioFile?.url)")
   }
 }
 
 struct ContentView: View {
-  @StateObject var conductor = RecorderConductor()
+
+  let conductor = RecorderConductor()
 
   var body: some View {
     VStack {
-      Spacer()
-      Text(conductor.data.isPlayingInput ? "STOP INPUT" : "PLAY INPUT").onTapGesture {
-        self.conductor.data.isPlayingInput.toggle()
-      }
-      Spacer()
-      Text(conductor.data.isRecording ? "STOP RECORDING" : "RECORD").onTapGesture {
-        self.conductor.data.isRecording.toggle()
-      }
-      Spacer()
-      Text(conductor.data.isPlayingRecording ? "STOP" : "PLAY RECORDING").onTapGesture {
-        self.conductor.data.isPlayingRecording.toggle()
-      }
-      Spacer()
+      Text("Hello World")
     }
 
     .padding()
     .navigationBarTitle(Text("Recorder"))
-    .onAppear {
-      self.conductor.start()
-    }
-    .onDisappear {
-      self.conductor.stop()
-    }
   }
 }
